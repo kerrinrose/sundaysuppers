15da2b5172820c3063a96c280d1cb3c402169848
Kerrin McLaughlin <nirrek@live.com>
2018-03-28 23:24:37 -0400

    Update wp-config.php

--- sunday-suppers-wordpress/wp-config.php
+++ sunday-suppers-wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'sundaysuppers');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', );
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', );
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
